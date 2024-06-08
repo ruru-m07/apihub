585810e17ac62467d053434171b5b03099276e87
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-06-08 14:06:28 +0530

    feat(theme): enhance dark mode support

--- examples/apps/chat-app/web/nextjs-app-router/app/page.tsx
+++ examples/apps/chat-app/web/nextjs-app-router/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/modeToggle";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Button</Button>
+      <ModeToggle />
     </main>
   );
 }
